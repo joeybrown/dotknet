cf0b43ba4150457254407ac11db11427d1449817
Joey <brownjn12@gmail.com>
2022-04-10 17:33:16 -0500

    use cli api instead of dragonfruit

--- src/Dotknet.Cli/Program.cs
+++ src/Dotknet.Cli/Program.cs
@@ -9,15 +9,33 @@ using System.CommandLine.Builder;
 using System.CommandLine.Hosting;
 using System.CommandLine.Parsing;
 using static LogEvents;
+using Serilog;
+using Serilog.Events;
 
 class Program
 {
   static async Task Main(string[] args) => await BuildCommandLine()
       .UseHost(_ => Host.CreateDefaultBuilder(), host =>
+      {
+        host.ConfigureLogging((context, config) =>
+        {
+          config.ClearProviders();
+          Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .MinimumLevel.Override("System", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+          config.AddSerilog(Log.Logger);
+        });
+
         host.ConfigureServices(services =>
         {
           services.AddSingleton<ILifecycle, Lifecycle>();
-        }))
+        });
+        
+      })
       .UseDefaults()
       .Build()
       .InvokeAsync(args);
@@ -25,9 +43,7 @@ class Program
   private static CommandLineBuilder BuildCommandLine()
   {
     var root = new RootCommand();
-
     var publish = Publish();
-
     root.Add(publish);
     return new CommandLineBuilder(root);
   }
@@ -43,15 +59,12 @@ class Program
     {
       var serviceProvider = host.Services;
       var lifecycle = serviceProvider.GetRequiredService<ILifecycle>();
-
       var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
       var logger = loggerFactory.CreateLogger(typeof(Program));
-
       var projectPath = options.ProjectPath;
       logger.LogInformation(PublishEvent, "Publish requested for: {project}", projectPath);
       lifecycle.Publish(options);
     });
-
     return command;
   }
 }
